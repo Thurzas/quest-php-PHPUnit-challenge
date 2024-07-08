6a6d51c284726995b17eb99e1ea7d6d26643062e
Thurzas <mathieu.miot@gmail.com>
2024-07-08 13:37:37 +0200

    phpUnit tests assertion

--- tests/CalculatorTest.php
+++ tests/CalculatorTest.php
@@ -22,4 +22,41 @@ class CalculatorTest extends TestCase
             "La méthode sum() ne fonctionne plus :-("
         );
     }
+    public function testMultiplication(): void
+    {
+        //Arrange
+        $firstValue = 5;
+        $secondValue = 8;
+        $expectedResult = 40;
+
+        //Act
+        $calculator = new Calculator();
+        $result = $calculator->multiplication($firstValue, $secondValue);
+        
+        //Assert
+        $this->assertSame($expectedResult, $result, 
+            "La méthode multiplication() ne fonctionne plus :-("
+        );
+    }
+    public function testIsEven(): void
+    {
+        //Arrange
+        $firstValue = 5;
+        $secondValue = 8;
+        //Act
+        $calculator = new Calculator();
+        $result = $calculator->multiplication($firstValue, $secondValue);
+        
+        $assertFirstValue = false; 
+        $this->assertFalse( 
+            $assertFirstValue, 
+            "La méthode isEven() ne fonctionne plus :-("
+        ); 
+
+        $assertSecondValue = true; 
+        $this->assertTrue( 
+            $assertSecondValue, 
+            "La méthode isEven() ne fonctionne plus :-("
+        ); 
+    }
 }
